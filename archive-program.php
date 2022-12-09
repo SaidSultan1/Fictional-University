ee76fd0ed47181ba5627318fcb8274f8c28efea4
SaidSultan1 <112623220+SaidSultan1@users.noreply.github.com>
2022-12-09 22:59:40 +0500

    Fictional Unviersity

--- archive-program.php
+++ archive-program.php
@@ -1,29 +1,34 @@
-<?php
-
+<?php 
 get_header();
 pageBanner(array(
-  'title' => 'All Programs',
-  'subtitle' => 'There is something for everyone. Have a look around.'
+  'title'=> 'All Programs',
+  'subtitle'=> 'All Program posts is here'
+
 ));
- ?>
+?>
 
-<div class="container container--narrow page-section">
 
-<ul class="link-list min-list">
+
+
+    <div class="container container--narrow page-section">
+        <ul class="link-list min-list">
+    <?php 
+
+
+
+    while(have_posts()){
+      the_post() ?>
+      <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+      <?php
+    }
+    
+    echo paginate_links()
+    
+    ?>
+    </ul>
+    
+  </div>
 
 <?php
-  while(have_posts()) {
-    the_post(); ?>
-    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-  <?php }
-  echo paginate_links();
-?>
-</ul>
-
-
-
-</div>
-
-<?php get_footer();
-
+get_footer();
 ?>
