385af756d4c998370291148b26fec03c418c94a7
SaidSultan1 <112623220+SaidSultan1@users.noreply.github.com>
2022-12-14 23:08:15 +0500

    Fictional Unviersity

--- functions.php
+++ functions.php
@@ -33,11 +33,14 @@ function pageBanner ($args = NULL) {
 function university_files() {
     wp_enqueue_style( 'font-family ','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
     wp_enqueue_style( 'font-awesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
-
     wp_enqueue_style('style-sheet', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('extra-styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_script('extra-script', get_theme_file_uri('/build/index.js'), array('jquery'),'1.0',true);
     wp_enqueue_script('googleMap','//maps.googleapi.com/maps/api/js?key=AIzaSyBmk41lhueG6v0cdzI8vwaFJAjgo8kW8Qk',NULL,'1.0',true);
+    wp_localize_script('getURL','universityData', array(
+        'root_url' => get_site_url()
+    ));
+
 
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "../css/style.scss"
 import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 import GoogleMap from "./modules/GoogleMap"
-import Search from "./modules/Search.js"
+ import Search from "./modules/Search.js"
 
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
 const heroSlider = new HeroSlider()
 const googleMap = new GoogleMap()
-const magicalSearch = new Search()
+ const magicalSearch = new Search()
 
 
